4c518b05a5fb53b18aab7e03f673604ec2f09b05
Kelvin Lin <kelvindlin97@gmail.com>
2021-05-26 14:21:21 -0400

    Setup: User links stored in db

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'alphabetically'
 
-@app.route('/')
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
+#TODO(Bugfix): About page routing
+@app.route('/#')
+def about():
+    return render_template('about.html')
+
+@app.route('/', methods=('GET', 'POST'))
 def index():
+    if request.method == 'POST':
+        link = request.form['link']
+
+        if not link:
+            flash('Link is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO links (link) VALUES (?)', (link,))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
     return render_template('index.html')
+
--- client/index.js
+++
@@ -1,3 +0,0 @@
-function run() {
-  return 'hi'
-}
