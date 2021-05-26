fd0f787d9a70d3ab50238c7756d852b49c2f8103
Kelvin Lin <kelvindlin97@gmail.com>
2021-05-26 12:53:11 -0400

    Setup: Form input box

--- form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    link = StringField('Link', validators=[DataRequired()])
-    submit = SubmitField('Submit Link')
