b267ff9d95a13667b82ae7c02b9a11dd8e27853c
Kelvin Lin <kelvindlin97@gmail.com>
2021-05-27 20:06:23 -0400

    Add google sheets API config

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'alphabetically'
@@ -14,14 +16,21 @@ def get_db_connection():
 def about():
     return render_template('about.html')
 
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('service_account.json', scope)
+client = gspread.authorize(creds)
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
     if request.method == 'POST':
         link = request.form['link']
-
         if not link:
             flash('Link is required!')
         else:
+            print(link)
+            # wks = client.open_by_url(link)
+            # val = wks.get_worksheet('A1').cell
+            # print(val)
             conn = get_db_connection()
             conn.execute('INSERT INTO links (link) VALUES (?)', (link,))
             conn.commit()
